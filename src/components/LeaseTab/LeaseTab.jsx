93952e80502502f7cb9d5c9c8db509f5cfc6ce9e
Vasily Gusev <vasily.guseff@gmail.com>
2020-02-09 15:33:12 +0300

    feat: add lease tab fields

--- src/components/LeaseTab/LeaseTab.jsx
+++ src/components/LeaseTab/LeaseTab.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const LeaseTab = () => {
-  return (
-    <div className="container pt-3">
-      <h4>Lease</h4>
-    </div>
-  )
+import SelectInput from '../SelectInput'
+
+class LeaseTab extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      term: 36,
+      creditScore: 750,
+      mileage: 12000,
+    }
+  }
+
+  changeTermHandle = e => {
+    e.preventDefault()
+    this.setState({ term: Number.parseInt(e.target.value, 10) })
+  }
+
+  changeCreditScoreChangeHandle = e => {
+    e.preventDefault()
+    this.setState({ creditScore: Number.parseInt(e.target.value, 10) })
+  }
+
+  changeMileageHandle = e => {
+    e.preventDefault()
+    this.setState({ mileage: Number.parseInt(e.target.value, 10) })
+  }
+
+  render() {
+    const termVars = [24, 36, 48]
+    const creditScores = [600, 650, 700, 750, 800, 850, 900]
+    const mileages = [10000, 12000, 15000]
+    const { term, creditScore, mileage } = this.state
+    return (
+      <div>
+        <SelectInput
+          text="Term for Lease"
+          prefix="term-lease"
+          value={term}
+          variables={termVars}
+          changeVarHandle={this.changeTermHandle}
+        />
+        <SelectInput
+          text="Annual Mileage"
+          prefix="mileage"
+          value={mileage}
+          variables={mileages}
+          changeVarHandle={this.changeMileageHandle}
+        />
+        <SelectInput
+          text="Credit Score"
+          prefix="score"
+          value={creditScore}
+          variables={creditScores}
+          changeVarHandle={this.changeCreditScoreChangeHandle}
+        />
+      </div>
+    )
+  }
 }
 
 export default LeaseTab
---
+++ src/components/SelectInput/SelectInput.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const SelectInput = ({ text, prefix, value, variables, changeVarHandle }) => {
+  return (
+    <div className="form-group row">
+      <div className="col-md-5">
+        <div className="">{text}</div>
+      </div>
+      <div className="col-md-7">
+        <select
+          className="form-control"
+          value={value}
+          id={prefix}
+          onChange={changeVarHandle}
+        >
+          {variables.map(x => (
+            <option key={x}>{x}</option>
+          ))}
+        </select>
+      </div>
+    </div>
+  )
+}
+
+SelectInput.propTypes = {
+  variables: PropTypes.arrayOf(PropTypes.number).isRequired,
+  changeVarHandle: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
+  prefix: PropTypes.string.isRequired,
+  value: PropTypes.number.isRequired,
+}
+
+export default SelectInput
